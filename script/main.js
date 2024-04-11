dc2372dc0957f8dd2725172a22c8df36be7d27f1
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2024-04-11 22:44:25 +0200

    .
    .

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+document.querySelector(".principal__code-area__text").spellcheck = false; // para remover a linha pontilhada vermelha da textarea
+
 const menuBurger = document.querySelector('.header__menu-icon'); 
 
 menuBurger.addEventListener('click', () => {
